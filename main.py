98ce2fd7149c8a1845a559f9048a2090edc188b4
vedanshu <anshgravity@gamil.com>
2022-08-10 22:22:46 +0530

    new data

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ ID = Intra_Day('NIFTY 50')
 dataPoints = ID.nifty_intraDay()
 today = datetime.today()
 
-with open(datetime.strftime(datetime(today.year, today.month, today.day), '%Y-%m-%d')+'.csv', 'w') as f:
+with open('/home/sort/ved/nse-tick-data/'+datetime.strftime(datetime(today.year, today.month, today.day), '%Y-%m-%d')+'.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerows(dataPoints)
