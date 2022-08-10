340bfabd8a8fa28ce1fcda8d60cbd49fc1a1bd0e
vedanshu <anshgravity@gamil.com>
2022-08-10 22:05:49 +0530

    new data

---
+++ run.sh
@@ -0,0 +1,5 @@
+python3 /home/sort/ved/nse-tick-data/main.py
+git -C /home/sort/ved/nse-tick-data add /home/sort/ved/nse-tick-data
+git -C /home/sort/ved/nse-tick-data commit -m 'new data'
+git -C /home/sort/ved/nse-tick-data push
+
